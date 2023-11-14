5d71132658e9399805aa5635d4fd250dc180c04b
nitin562 <nitindbas8800@gmail.com>
2023-11-14 18:37:15 +0530

    change db uri

--- backend/ConnectDB.js
+++ backend/ConnectDB.js
@@ -1,6 +1,6 @@
 const db=require("mongoose")
 const connect=async()=>{
-    await db.connect("mongodb://127.0.0.1:27017/")
+    await db.connect('mongodb://127.0.0.1:27017/?appName=MongoDB+Compass&directConnection=true&serverSelectionTimeoutMS=2000')
 }
 
 module.exports=connect
