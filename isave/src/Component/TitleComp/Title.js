eedb28ba0641063bab17dbf022631cd3ce431e7d
nitin562 <nitindbas8800@gmail.com>
2023-11-19 12:21:36 +0530

    Modify

--- isave/src/Component/TitleComp/Title.js
+++ isave/src/Component/TitleComp/Title.js
@@ -4,7 +4,7 @@ export default function Title({title,icon,click,clas,text}) {
 
   return (
     <div id="titleBox">
-        <p>{title.length>40?title.slice(0,41)+"...":title}</p>
+        <p>{title.length>15?title.slice(0,16)+"...":title}</p>
         {icon &&<i title={icon} onClick={click} className={clas}></i>}
         {text&&<span onClick={click?click:()=>{}}>{text}</span>}
     </div>
